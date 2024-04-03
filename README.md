9ed258eff358f36c002ed0e5fdd59bcf8c510e34
Aamirkadivar <amirkadivar@yahoo.com>
2024-04-03 21:58:55 +0330

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	deleted:    .gitattributes
    	deleted:    .gitignore
    	deleted:    README.md
    	deleted:    Status_report.md
    	deleted:    db.sqlite3
    	deleted:    home/__init__.py
    	deleted:    home/admin.py
    	deleted:    home/apps.py
    	deleted:    home/migrations/__init__.py
    	deleted:    home/models.py
    	deleted:    home/templates/home/main.html
    	deleted:    home/tests.py
    	deleted:    home/urls.py
    	deleted:    home/views.py
    	deleted:    manage.py
    	deleted:    media/images/333.png
    	deleted:    media/images/Untitled.png
    	deleted:    media/images/goraz2.png
    	deleted:    media/images/goraz2_HFoIDfU.png
    	deleted:    media/images/goraz2_Vxsep2P.png
    	deleted:    media/images/goraz2_cLxs5Zr.png
    	deleted:    myproject/__init__.py
    	deleted:    myproject/asgi.py
    	deleted:    myproject/settings.py
    	deleted:    myproject/urls.py
    	deleted:    myproject/wsgi.py
    	deleted:    requirements.txt
    	deleted:    resources.txt
    	deleted:    signup/__init__.py
    	deleted:    signup/admin.py
    	deleted:    signup/apps.py
    	deleted:    signup/forms.py
    	deleted:    signup/migrations/0001_initial.py
    	deleted:    signup/migrations/__init__.py
    	deleted:    signup/models.py
    	deleted:    signup/static/3456684.png
    	deleted:    signup/static/apple.png
    	deleted:    signup/static/style_signup.css
    	deleted:    signup/templates/signup/login.html
    	deleted:    signup/templates/signup/password_reset.html
    	deleted:    signup/templates/signup/password_reset_complete.html
    	deleted:    signup/templates/signup/password_reset_confirm.html
    	deleted:    signup/templates/signup/password_reset_email.html
    	deleted:    signup/templates/signup/password_reset_subject.txt
    	deleted:    signup/templates/signup/profile.html
    	deleted:    signup/templates/signup/signup.html
    	deleted:    signup/tests.py
    	deleted:    signup/urls.py
    	deleted:    signup/views.py
    	deleted:    static/style.css

--- README.md
+++
@@ -1,3 +0,0 @@
-Directory for entire Django website
-# Useful Commands
-- `python manage.py runserver`
--- home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path,include
-from . import views
-from django.contrib.auth import views as auth_views
-
-urlpatterns = [
-    path('', views.HomeView.as_view(),name= "home"),
-    # path("accounts/", include("django.contrib.auth.urls")),
-]
